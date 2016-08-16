dc0d835830b42cc3a8eab55dfcc63b030bc80d68
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2016-08-16 10:23:18 -0500

    Cambios al settings.py

--- olimpicolombia/settings.py
+++ olimpicolombia/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = "olimpicolombia/static"
+STATIC_ROOT = "olimpicolombia01/static"
 
 STATIC_URL = '/static/'
 
