e21390a8adf610c6acc33ee62f307f6a651164f4
José Miguel <jm.suarez201@uniandes.edu.co>
2016-08-23 09:24:04 -0500

    Mojora en el mockup de deportistas y mejora en el botón para ver eventos de un deportista

--- olimpicolombiaApp/static/app/models/deportista-model.js
+++ olimpicolombiaApp/static/app/models/deportista-model.js
@@ -2,11 +2,7 @@
 
     var app = angular.module('app');
 
-    app.factory('Deportista', function($resource) {
-        var resource = $resource('/api/deportes/:deporteId/deportistas');
-        var model = {};
-        model.query = function(params) {
-            return [{
+    var mockupDeportistasService = [{
                 id: 1,
                 idDeporte: 1,
                 nombre: "Caterine Ibargüen",
@@ -19,22 +15,16 @@
                 urlImagen: 'http://www.elheraldo.co/sites/default/files/2015/08/24/articulo/caterine_ibarguen_2.jpg',
                 urlVideo: 'https://www.youtube.com/embed/wvZpnSPUy34'
             }];
+
+    app.factory('Deportista', function($resource) {
+        var resource = $resource('/api/deportes/:deporteId/deportistas');
+        var model = {};
+        model.query = function(params) {
+            return mockupDeportistasService;
         };
         model.get = function (params) {
-            return {
-                id: 1,
-                idDeporte: 1,
-                nombre: "Caterine Ibargüen",
-                lugarNacimiento: "Lugar",
-                fechaNacimiento: new Date("1984-07-23"),
-                edad: 23,
-                peso: 67,
-                estatura: 1.85,
-                entrenador: 'Julio',
-                urlImagen: 'http://www.elheraldo.co/sites/default/files/2015/08/24/articulo/caterine_ibarguen_2.jpg',
-                urlVideo: 'https://www.youtube.com/embed/wvZpnSPUy34'
-            };
-        }
+            return mockupDeportistasService[0];
+        };
         return model;
     });
 
