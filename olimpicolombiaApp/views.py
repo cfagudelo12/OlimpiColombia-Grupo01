44004aaa72457914a15a11b35b3fd9301b86ac32
José Miguel <jm.suarez201@uniandes.edu.co>
2016-08-27 21:44:53 -0500

    Se permite hacer registro así se esté logueado como administrador

--- olimpicolombiaApp/views.py
+++ olimpicolombiaApp/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
@@ -39,6 +39,8 @@ def lista_eventos(request, idDeportista):
     return Response(serializer.data)
 
 @api_view(['POST'])
+@authentication_classes(())
+@permission_classes(())
 def add_user_view(request):
     serializer = UsuarioSerializer(data=request.data)
     if serializer.is_valid():
