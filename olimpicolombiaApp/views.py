825c6fee2f70cec860be27d5f8ed70c26236d6fc
José Miguel <jm.suarez201@uniandes.edu.co>
2016-08-27 22:07:35 -0500

    No se permite a usuarios no logueados mirar los deportistas ni los eventos

--- olimpicolombiaApp/views.py
+++ olimpicolombiaApp/views.py
@@ -1,10 +1,8 @@
 from rest_framework import status
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-
-
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import DeporteSerializer, DeportistaSerializer, EventoSerializer, UsuarioSerializer
 from .models import Deporte, Deportista, Evento
@@ -16,24 +14,32 @@ def lista_deportes(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def deporte (request, idDeporte):
     deporte = Deporte.objects.get(id=idDeporte)
     serializer = DeporteSerializer(deporte)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def lista_deportistas(request, idDeporte):
     deportistas = Deportista.objects.filter(deporte=idDeporte)
     serializer = DeportistaSerializer(deportistas, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def deportista(request, idDeportista):
     deportista = Deportista.objects.get(id=idDeportista)
     serializer = DeportistaSerializer(deportista)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def lista_eventos(request, idDeportista):
     eventos=Evento.objects.filter(deportista=idDeportista)
     serializer = EventoSerializer(eventos, many=True)
