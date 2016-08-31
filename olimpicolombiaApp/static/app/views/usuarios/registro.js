b554f2f4e678cfc5c9ea4b3b1476ff695e76df98
cdmontanezg <cedamogue@gmail.com>
2016-08-30 21:55:12 -0500

    validacion del resultado del llamado al servicio rest de registro de usuarios

--- olimpicolombiaApp/static/app/views/usuarios/registro.js
+++ olimpicolombiaApp/static/app/views/usuarios/registro.js
@@ -6,14 +6,22 @@
 	var app = angular.module('app');
 
 	app.controller('RegistroCtrl', function($scope, Usuario) {
-	    console.log("asdasd")
+
 		$scope.user = {}
         
 		$scope.registrar = function (user) {
             var usuario = new Usuario(user)
-
-            usuario.$save()
-        }
+			$scope.exito = {'mensaje':''}
+			$scope.error = {'mensaje':''}
+            usuario.$save(
+            	function(data) {
+            		$scope.exito = {'mensaje':'Guardado con éxito'}
+				},
+				function(error) {
+            		$scope.error = {'mensaje':'Error en el formulario!!'}
+				})
+			return
+		}
 	});
 
 })();
